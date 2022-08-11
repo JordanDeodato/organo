f93f47e222231cfb97dd3b5aec1f0d66e62231cc
Jordan Deodato dos Santos <100080781+JordanDeodato@users.noreply.github.com>
2022-08-11 11:56:03 -0300

    Update index.js

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -6,13 +6,13 @@ const Rodape = () => {
             <div>
                 <img alt="icone fb" src="./imagens/fb.png" />
                 <img alt="icone tt" src="./imagens/tw.png" />
-                <img alt="icone ig" src="./imagens/ig.png" />
+                <a href="https://www.instagram.com/jordan_deodato/"><img alt="icone ig" src="./imagens/ig.png" /></a>
             </div>
             <div className='logo-rodape'>
                 <img alt="logo" src="imagens/banner-logo.png" />
             </div>
             <div>
-                <p>Desenvolvido por Jordan Deodato</p>
+                <p>Desenvolvido por <a href="https://www.linkedin.com/in/jordandeodato/" target="blank">Jordan Deodato</a></p>
             </div>
         </div>
     )
